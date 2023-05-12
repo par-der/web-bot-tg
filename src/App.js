ab0233f50332367eb1237e1141a8e7b2dd63f234
Терентьев Егор <e.terentyev@ruskon.local>
2023-05-12 09:54:15 +0400

    use hooks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import { useEffect } from 'react';
 import { useTelegram } from './hooks/useTelegram';
-
+import Header from '.components/Header/Header'
 
 function App() {
   const {onToggleButton, tg} = new useTelegram();
-  
+
   useEffect(() => {
     tg.ready();
   }, [])
@@ -14,6 +14,7 @@ function App() {
 
   return (
     <div className="App">
+      <Header/>
       <button onClick={onToggleButton}>Toggle</button>
     </div>
   );
